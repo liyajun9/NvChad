2adf68273ca7f6676a11db5a3a2b66ae3408600e
liyajun9 <liyajun9@126.com>
2026-08-14 16:08:21 +0800

    add <leader>ca

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -41,6 +41,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "<leader>cR", rename, rename_opts)
     vim.keymap.set("n", "<leader>rn", rename, rename_opts)
     vim.keymap.set("n", "<leader>ra", rename, rename_opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {
+      buffer = args.buf,
+      desc = "LSP Code Action",
+    })
 
     if client and client.name == "clangd" then
       vim.keymap.set("n", "<leader>ch", "<cmd>LspClangdSwitchSourceHeader<cr>", {
